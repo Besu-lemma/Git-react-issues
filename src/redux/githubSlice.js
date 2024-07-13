fe7a8ee162b414c88de88cce7c92a4fb6a548840
besu-lemma <besufikadle95@gmail.com>
2024-07-13 10:40:10 +0300

    large-file-commit

--- src/redux/githubSlice.js
+++ src/redux/githubSlice.js
@@ -1,52 +1,13 @@
+// src/redux/githubSlice.js
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-/**
- * @typedef {Object} User
- * @property {string} login
- * @property {string} avatar_url
- * @property {number} id
- */
+import { Octokit } from "@octokit/core";
 
-/**
- * @typedef {Object} IssueLabels
- * @property {number} id
- * @property {string} name
- * @property {string} color
- */
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
 
-/**
- * @typedef {Object} ErrorState
- * @property {boolean} status
- * @property {string|null} message
- */
-
-/**
- * @typedef {Object} Issue
- * @property {number} id
- * @property {User} user
- * @property {string} state
- * @property {number} number
- * @property {number} comments
- * @property {string} title
- * @property {string} html_url
- * @property {Date} created_at
- * @property {IssueLabels[]} labels
- */
-
-/**
- * @typedef {Object} GitHubState
- * @property {Issue[]} issues
- * @property {boolean} loading
- * @property {number} totalPages
- * @property {number} currentPage
- * @property {string} search
- * @property {ErrorState} error
- */
-// githubSlice.js
-
-const GITHUB_API_URL = "https://api.github.com";
-const PERSONAL_ACCESS_TOKEN =
-  "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";
+const perPage = 10;
 
 const initialState = {
   issues: [],
@@ -61,32 +22,30 @@ export const fetchIssues = createAsyncThunk(
   "github/fetchIssues",
   async ({ page, search }, { rejectWithValue }) => {
     try {
-      const response = await fetch(
-        `${GITHUB_API_URL}/search/issues?q=${search}&page=${page}&per_page=10`,
-        {
-          headers: {
-            Authorization: `token ${PERSONAL_ACCESS_TOKEN}`,
-          },
-        }
-      );
+      const response = await octokit.request("GET /search/issues", {
+        q: search,
+        page: page,
+        per_page: perPage,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      });
 
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-
-      const responseData = await response.json();
-      const data = responseData.items;
       const totalPages =
-        responseData.total_count < 1000
-          ? Math.ceil(responseData.total_count / 10)
-          : Math.ceil(1000 / 10);
+        response.data.total_count < 1000
+          ? Math.ceil(response.data.total_count / perPage)
+          : Math.ceil(1000 / perPage);
 
-      return { issues: data, totalPages, currentPage: page, search };
+      return {
+        issues: response.data.items,
+        totalPages,
+        currentPage: page,
+        search,
+      };
     } catch (error) {
-      console.error("Error fetching GitHub issues:", error);
       return rejectWithValue({
         status: true,
-        message: error instanceof Error ? error.message : "Unknown Error",
+        message: error.message || "Unknown Error",
       });
     }
   }
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  resolve: {
+    fallback: {
+      os: require.resolve("os-browserify/browser"),
+    },
+  },
+  entry: "./src/index.js", // Adjust the entry point as needed
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new Dotenv()],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+};
